abc0ae9dd9409d347fffd8f2ed6e4c2972683f47
hiren1995 <kadamhiren.sigma@gmail.com>
2018-07-12 12:47:17 +0530

    Initial Commit

---
+++ Maa ShaktiTests/Maa_ShaktiTests.swift
@@ -0,0 +1,36 @@
+//
+//  Maa_ShaktiTests.swift
+//  Maa ShaktiTests
+//
+//  Created by Apple on 12/07/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import XCTest
+@testable import Maa_Shakti
+
+class Maa_ShaktiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
